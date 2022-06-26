5c335d38de97f816d668f17e98b7c5b3144f54f9
gradgrind <mt.bothfeld@gmail.com>
2022-06-26 21:57:57 +0200

    Start work on course_analysis module (activity lists)

---
+++ program/timetable/course_analysis.py
@@ -0,0 +1,286 @@
+"""
+timetable/courses.py
+
+Last updated:  2022-06-26
+
+Collect information on activities for teachers and classes/groups.
+
+=+LICENCE=============================
+Copyright 2022 Michael Towers
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+=-LICENCE========================================
+"""
+
+#TODO: Move to translations:
+_SUPPRESSED = "Lehrkraft ausgeschlossen: {tname}"
+
+###############################################################
+
+if __name__ == "__main__":
+    # Enable package import if running as module
+    import sys, os
+
+    this = sys.path[0]
+    appdir = os.path.dirname(this)
+    sys.path[0] = appdir
+    basedir = os.path.dirname(appdir)
+    from core.base import start
+
+    # start.setup(os.path.join(basedir, "TESTDATA"))
+    start.setup(os.path.join(basedir, "DATA-2023"))
+
+T = TRANSLATIONS("timetable.courses")
+
+### +++++
+
+from typing import NamedTuple, Optional
+
+from io import BytesIO
+from reportlab.platypus import SimpleDocTemplate, Paragraph, PageBreak, Spacer
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import mm
+
+from core.db_management import db_read_fields
+from core.basic_data import get_classes, get_teachers
+from timetable.courses import CourseData, blocktag2blocksid
+
+### -----
+
+
+def get_course_info():
+    """Return course information for classes and teachers.
+    In particular, this information can be used to build activity tables
+    for teachers and classes/groups.
+    """
+    # Get group info for checking groups
+    __classes = get_classes()
+    class2groups = {
+        klass: __classes.group_info(klass)["GROUPS"]
+        for klass, _ in __classes.get_class_list()
+    }
+
+    course2data = {}
+    class2courses = {}
+    teacher2courses = {}
+    for course, klass, group, sid, tid in db_read_fields(
+        "COURSES", ("course", "CLASS", "GRP", "SUBJECT", "TEACHER")
+    ):
+        # CLASS, SUBJECT and TEACHER are foreign keys and should be
+        # automatically bound to appropriate entries in the database.
+        # GRP should be checked here ...
+        if group and group not in class2groups[klass]:
+            if klass != "--" and group != "*":
+                SHOW_ERROR(
+                    T["UNKNOWN_GROUP"].format(
+                        klass=klass, group=group, sid=sid, tid=tid
+                    )
+                )
+                continue
+        course2data[course] = CourseData(
+            klass=klass, group=group, sid=sid, tid=tid
+        )
+        try:
+            class2courses[klass].append(course)
+        except KeyError:
+            class2courses[klass] = [course]
+        if tid and tid != "--":
+            try:
+                teacher2courses[tid].append(course)
+            except KeyError:
+                teacher2courses[tid] = [course]
+
+    course2payroll = {}     # {course-id -> [payroll, ... ]}
+    course2block = {}       # {course-id -> [(block-tag, payroll), ... ]}
+    course2plain = {}       # {course-id -> [(length, payroll), ... ]}
+    block_sublessons = {}   # {block-tag -> [length, ... ]}
+    for id, course, length, payroll, time, place in db_read_fields(
+        "LESSONS",
+        ("id", "course", "LENGTH", "PAYROLL", "TIME", "PLACE"),
+    ):
+        if course:
+            if length == "--" and not time:
+                ## non-lesson
+                try:
+                    course2payroll[course].append(payroll)
+                except KeyError:
+                    course2payroll[course] = [payroll]
+#TODO: Is it at all sensible to support multiple such entries for a course?
+
+            elif length == "*" and time.startswith(">"):
+                ## block-member
+                val = (time, payroll)
+                try:
+                    course2block[course].append(val)
+                except KeyError:
+                    course2block[course] = [val]
+
+            else:
+                try:
+                    l = int(length)
+                except ValueError:
+                    raise Bug(f"LESSON id={id}, LENGTH field is not a number: {length}")
+                if time and time[0] in "@=":
+                    ## plain lesson
+                    val = (l, payroll)
+                    try:
+                        course2plain[course].append(val)
+                    except KeyError:
+                        course2plain[course] = [val]
+                else:
+                    raise Bug(f"Invalid LESSON, id={id}: LENGTH={length}, TIME={time}")
+
+        elif place.startswith(">"):
+            ## block-sublesson: add the length to the list for this block-tag
+            try:
+                i = int(length)
+                block_sublessons[place].append(i)
+            except KeyError:
+                block_sublessons[place] = [i]
+            except ValueError:
+                raise Bug(f"Invalid block-sublesson: block-tag={place}, length={length}")
+
+        elif not place.startswith("="):
+            ## partner-time is ignored, anything else is a bug
+            raise Bug(f"LESSON id={id}, error in PLACE field: {place}")
+
+    return {
+        "COURSE2DATA":    course2data,      # {course-id -> CourseData}
+        "CLASS2COURSES":  class2courses,    # {class -> {course-id, ... ]}
+        "TEACHER2COURSES": teacher2courses, # {tid -> {course-id, ... ]}
+        "COURSE2PAYROLL": course2payroll,   # {course-id -> [payroll, ... ]}
+        "COURSE2BLOCK":   course2block,     # {course-id -> [(block-tag, payroll), ... ]}
+        "COURSE2PLAIN":   course2plain,     # {course-id -> [(length, payroll), ... ]}
+        "BLOCK_SUBLESSONS": block_sublessons # {block-tag -> [length, ... ]}
+    }
+
+
+def blockdata(course_info, course):
+    for blocktag, payroll in course_info["COURSE2BLOCK"]:
+        blocksid = blocktag2blocksid(blocktag)
+        sublessons = course_info["BLOCK_SUBLESSONS"][blocktag]
+#TODO ...
+
+
+#TODO ...
+# Include ROOM field?
+def teacher_class_subjects(course_info, block_tids=None):
+    """For each teacher, present the subjects/courses together with
+    groups, rooms etc.
+    If <block_tids> is supplied, it should be a set of teacher-ids which
+    will be "blocked", i.e. not appear in the output.
+    Build a list of "pages", one for each teacher, with a list of his/her
+    classes and subjects.
+    """
+    teachers = get_teachers()
+    tid2courses = course_info["TEACHER2COURSES"]
+    course2data = course_info["COURSE2DATA"]
+    if block_tids is None:
+        block_tids = set()
+    tlist = []
+    for tid in teachers:
+        try:
+            courses = tid2courses[tid]
+        except KeyError:
+            ## teacher has no entries
+            continue
+        if tid in block_tids:
+            REPORT("INFO", _SUPPRESSED.format(tname=tname))
+            continue
+        tname = teachers.name(tid)
+        for course in courses:
+            cdata = course2data[course]
+
+
+    for tid, tname, clist in teacher_subjects:
+        if tid in block_tids:
+            REPORT("INFO", _SUPPRESSED.format(tname=tname))
+            continue
+        else:
+            tlist.append((f"{tname} ({tid})", clist))
+
+    pdf = PdfCreator()
+    return pdf.build_pdf(tlist, title="Lehrer-Klassen-Fächer",
+            author="FWS Bothfeld")
+
+
+BASE_MARGIN = 20 * mm
+#TODO
+class PdfCreator:
+    def add_page_number(self, canvas, doc):
+        canvas.saveState()
+        canvas.setFont('Times-Roman', 12)
+        page_number_text = "%d" % (doc.page)
+        canvas.drawCentredString(
+            18 * mm,
+            18 * mm,
+            page_number_text
+        )
+        canvas.restoreState()
+
+    def build_pdf(self, pagelist, title, author):
+        pdf_buffer = BytesIO()
+        my_doc = SimpleDocTemplate(
+            pdf_buffer,
+            title=title,
+            author=author,
+            pagesize=A4,
+            topMargin=BASE_MARGIN,
+            leftMargin=BASE_MARGIN,
+            rightMargin=BASE_MARGIN,
+            bottomMargin=BASE_MARGIN
+        )
+        sample_style_sheet = getSampleStyleSheet()
+        body_style = sample_style_sheet['BodyText']
+        body_style.fontSize = 14
+        body_style.leading = 20
+        heading_style = sample_style_sheet['Heading1']
+        heading_style.spaceAfter = 24
+        class_style = sample_style_sheet['Heading2']
+        class_style.spaceBefore = 25
+        #print("\n STYLES:", sample_style_sheet.list())
+
+        flowables = []
+        for teacher, clist in pagelist:
+            flowables.append(Paragraph(teacher, heading_style))
+            for klass, slist in clist:
+                flowables.append(Paragraph(klass, class_style))
+                for subject in slist:
+                    if subject:
+                        flowables.append(Paragraph(subject, body_style))
+                    else:
+                        flowables.append(Spacer(1, 4 * mm))
+            flowables.append(PageBreak())
+        my_doc.build(
+            flowables,
+            onFirstPage=self.add_page_number,
+            onLaterPages=self.add_page_number,
+        )
+        pdf_value = pdf_buffer.getvalue()
+        pdf_buffer.close()
+        return pdf_value
+
+
+# --#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#
+
+if __name__ == "__main__":
+    from core.db_management import open_database
+    open_database()
+
+    course_info = get_course_info()
+    class2courses = course_info["CLASS2COURSES"]
+    for c in sorted(class2courses):
+        info = class2courses[c]
+        print(f"\nCLASS {c}:")
+        print("  ...", info)
